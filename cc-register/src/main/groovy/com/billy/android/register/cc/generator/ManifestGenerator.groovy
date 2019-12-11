13234447e6d5391eacd22ee27756b65e36360958
qiyi <qiyi@kaishiba.com>
2019-12-11 16:29:05 +0800

    解决gradle5.0版本以上报WARNING的问题

--- cc-register/src/main/groovy/com/billy/android/register/cc/generator/ManifestGenerator.groovy
+++ cc-register/src/main/groovy/com/billy/android/register/cc/generator/ManifestGenerator.groovy
@@ -31,8 +31,20 @@ class ManifestGenerator {
         android.applicationVariants.all { variant ->
             String pkgName = [variant.mergedFlavor.applicationId, variant.mergedFlavor.applicationIdSuffix, variant.buildType.applicationIdSuffix].findAll().join()
             variant.outputs.each { output ->
-                output.processManifest.doLast {
-                    output.processManifest.outputs.files.each { File file ->
+                def processManifest = null
+                def gradleAbove5 = GradleVersion.version(project.gradle.gradleVersion) >= GradleVersion.version('5.0.0')
+                //fix warning:
+                //  WARNING: API 'variantOutput.getProcessManifest()' is obsolete and has
+                //  been replaced with 'variantOutput.getProcessManifestProvider()'.
+                //  It will be removed at the end of 2019.
+                if (gradleAbove5) {
+                    processManifest = output.processManifestProvider.get()
+                }
+                if(processManifest == null) {
+                    processManifest = output.processManifest
+                }
+                processManifest.doLast {
+                    processManifest.outputs.files.each { File file ->
                         //在gradle plugin 3.0.0之前，file是文件，且文件名为AndroidManifest.xml
                         //在gradle plugin 3.0.0之后，file是目录，AndroidManifest.xml文件在此目录下
                         def manifestFile = null
